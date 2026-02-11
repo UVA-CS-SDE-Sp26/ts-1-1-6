091f78d78746f2170710262908574feccfe6d576
tristan-hernandez <fyp4nm@virginia.edu>
2026-02-10 22:01:36 -0500

    Started writing tests for UserInterface.java

--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -1,13 +1,21 @@
 import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInterfaceTest {
+    ProgramController programController;
+
+    @BeforeEach
+    void setup() {
+        programController = mock(ProgramController.class);
+        when(programController.getFileList()).thenReturn("01 filea.txt\n" + "02 fileb.txt\n" + "03 filec.txt");
+    }
 
     @Test
     void getReturnMessageNoArgs() {
         String[] args = {};
-        ProgramController programController = mock(ProgramController.class);
         UserInterface userInterface = new UserInterface(args, programController);
     }
 }
