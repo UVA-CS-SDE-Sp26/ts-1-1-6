0394d46d00cd7eb58b7ae6d67a10438b8eaaedb2
ThatOneGuyIDK <thomas2006vp@outlook.com>
2026-02-10 22:32:00 -0500

    build test repo and add test files

--- src/test/FilehandlerTEST.java
+++ src/test/FilehandlerTEST.java
@@ -1,14 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+//makes interface to test the methods that file handler uses
+interface MockCipher {
+    String decipher(String cipherText, String optionalKeyPath);
+}
+
+
 class FilehandlerTEST {
+
+
+    private String testResourcePath;
+    private FileHandler fileHandler;
+    private MockCipher mockCipher;
+
+
     String[] TestListOfFileNames = {"Document1.txt", "SuperSecretSpyDoc.txt", "123_That's_Classified!.txt", "For The President's Eyes Only.txt", "Ciphered.txt"}
 
+
+
+    @BeforeEach
+    void setUp() throws IOException {
+        testResourcePath = "";
+        //uses mock Cipher class and sets the test directory
+        fileHandler = new FileHandler(mockCipher, testResourcePath);
+    }
 
     @Test
     void integerArgumentsGiven(){
