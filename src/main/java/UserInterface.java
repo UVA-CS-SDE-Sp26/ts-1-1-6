afb66877bddde3e74b4a98a446e809e961046bfb
tristan-hernandez <fyp4nm@virginia.edu>
2026-02-12 13:39:30 -0500

    Wrote UserInterface tests and implemented UserInterface

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -8,6 +8,17 @@ public class UserInterface {
     }
 
     public String getReturnMessage() {
-        return "";
+        if (args.length == 0) {
+            return programController.getFileList();
+        }
+        else if (args.length == 1) {
+            return programController.handleFileRequest(args[0], null);
+        }
+        else if (args.length == 2) {
+            return programController.handleFileRequest(args[0], args[1]);
+        }
+        else {
+            return "Error: Too many arguments. Pass up to two arguments.";
+        }
     }
 }
--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -11,12 +11,72 @@ class UserInterfaceTest {
     void setup() {
         programController = mock(ProgramController.class);
         when(programController.getFileList()).thenReturn("01 filea.txt\n" + "02 fileb.txt\n" + "03 filec.txt");
+        when(programController.handleFileRequest("01", null)).thenReturn("Contents of file 01.");
+        when(programController.handleFileRequest("04", null)).thenReturn("Error: Invalid file number.");
+        when(programController.handleFileRequest("01", "alt_key.txt")).thenReturn("Deciphered contents of file 01 according to alt_key.txt.");
     }
 
     @Test
     void getReturnMessageNoArgs() {
         String[] args = {};
         UserInterface userInterface = new UserInterface(args, programController);
+        String expected = "01 filea.txt\n" + "02 fileb.txt\n" + "03 filec.txt";
+        String actual = userInterface.getReturnMessage();
+        assertEquals(expected, actual, "Passing no arguments should yield a list of files");
+    }
+
+    @Test
+    void getReturnMessageOneArgument() {
+        String[] args = {"01"};
+        UserInterface userInterface = new UserInterface(args, programController);
+        String expected = "Contents of file 01.";
+        String actual = userInterface.getReturnMessage();
+        assertEquals(expected, actual, "Passing one valid argument should yield file contents.");
+    }
+
+    @Test
+    void getReturnMessageOneArgumentInvalidIndex() {
+        String[] args = {"04"};
+        UserInterface userInterface = new UserInterface(args, programController);
+        String expected = "Invalid Index";
+        String actual = userInterface.getReturnMessage();
+        assertEquals(expected, actual, "Passing an invalid index should yield an error.");
+    }
+
+    @Test
+    void getReturnMessageOneArgumentInvalidNumber() {
+        String[] args = {"a"};
+        UserInterface userInterface = new UserInterface(args, programController);
+        String expected = "Error: Invalid file number.";
+        String actual = userInterface.getReturnMessage();
+        assertEquals(expected, actual, "Passing an invalid number should yield an error.");
+    }
+
+    @Test
+    void getReturnMessageTwoArguments() {
+        String[] args = {"01", "alt_key.txt"};
+        UserInterface userInterface = new UserInterface(args, programController);
+        String expected = "Deciphered contents of file 01 according to alt_key.txt.";
+        String actual = userInterface.getReturnMessage();
+        assertEquals(expected, actual, "Passing two arguments should decipher file contents according to alternate key.");
+    }
+
+    @Test
+    void getReturnMessageAlternateKeyVerification() {
+        String[] args = {"01", "alt_key.txt"};
+        UserInterface userInterface = new UserInterface(args, programController);
+        String unexpected = "Contents of file 01.";
+        String actual = userInterface.getReturnMessage();
+        assertNotEquals(unexpected, actual, "Passing an alternate key should decipher file contents.");
+    }
+
+    @Test
+    void getReturnMessageTooManyArguments() {
+        String[] args = {"01", "alt_key.txt", "extra"};
+        UserInterface userInterface = new UserInterface(args, programController);
+        String unexpected = "Error: Too many arguments. Pass up to two arguments.";
+        String actual = userInterface.getReturnMessage();
+        assertNotEquals(unexpected, actual, "Passing more than two arguments should yield an error.");
     }
 }
 
