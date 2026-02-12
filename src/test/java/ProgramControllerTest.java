c820dd0b525d61722bed1175a45d6b85fade3a23
brahm <brahmjotchawla@gmail.com>
2026-02-12 00:04:37 -0500

    correctly made sure they test so gradle runs it in git bash

--- src/test/java/ProgramControllerTest.java
+++ src/test/java/ProgramControllerTest.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProgramControllerTest {
+    @Test
     public void testGetFileListFormatting() { //testing if the format of the list displayed is as expected by Pat (starting with 01, 02, 03...)
         ProgramController controller = new ProgramController();
         String result = controller.getFileList();
@@ -10,11 +13,13 @@ public class ProgramControllerTest {
             assertEquals("No Files Found", result);
         }
     }
+    @Test
     public void testHandleFileRequestInvalidFormat() { //checks if the handleFileRequest returns the expected error if it isn't given a number
         ProgramController controller = new ProgramController();
         String result = controller.handleFileRequest("abc", null);
         assertEquals("Error: Invalid file number.", result);
     }
+    @Test
     public void testHandleFileRequestWithAlternateKey() { //tests handleFileRequest with a key
         ProgramController controller = new ProgramController();
         String customKey = "abcdefghijklmnopqrstuvwxyz\nzyxwvutsrqponmlkjihgfedcba";
@@ -22,6 +27,7 @@ public class ProgramControllerTest {
         assertNotNull(result);
         assertNotEquals("Invalid Index", result);
     }
+    @Test
     public void testHandleFileRequestWithoutAlternateKey() { //tests handleFileRequest without a key
         ProgramController controller = new ProgramController();
         String result = controller.handleFileRequest("1", null);
