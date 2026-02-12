36282791ad115552d1911c10d668a9c00c891518
brahm <brahmjotchawla@gmail.com>
2026-02-11 23:10:03 -0500

    Finished getFIleList

--- src/main/java/ProgramController.java
+++ src/main/java/ProgramController.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class ProgramController {
+    private FileHandler fileHandler;
+    private Cipher cipher;
+
+    public ProgramController() {
+        this.fileHandler = new FileHandler();
+        this.cipher = new Cipher();
+    }
+    public String getFileList() {
+        ArrayList<String> files = fileHandler.listFiles();
+        if(files.isEmpty()) {
+            return "No Files Found";
+        }
+        else {
+            StringBuilder fileList = new StringBuilder();
+            int i = 1;
+            for (String file : files) {
+                fileList.append(String.format("%02d %s\n", i++, file));
+            }
+            return fileList.toString();
+        }
+    }
 }
